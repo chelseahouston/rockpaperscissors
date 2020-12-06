60729fa9d17d179a9ac61693d1158225b51b9fd9
Chelsea Houston <chelseahouston29@gmail.com>
2020-12-06 20:27:45 +0000

    added break to while loop to end game

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
 # ROCK PAPER SCISSORS GAME ---
 def main():
-  # NAME INPUT ---
+  # NAME ---
   player1 = input("Player 1, what is your name? ")
   player2 = input("Player 2, what is your name? ")
-  # ANSWER INPUT ---
-  p1a = input(f"{player1}, rock, paper or scissors? ").lower()
-  p2a = input(f"{player2}, rock, paper or scissors? ").lower()
 
-  # COMPARE ANSWERS ---
+  # ANSWER INPUT ---
+ 
+  p1a = input(f"{player1}, rock, paper or scissors? ").lower()
+  while p1a not in ('rock', 'paper', 'scissors'):
+    print("Incorrect value.")
+    break
+  
+  p2a = input(f"{player2}, rock, paper or scissors? ").lower()
+  if p2a not in ('rock', 'paper', 'scissors'):
+    print("Incorrect value.")
+      
+ # COMPARE ANSWERS ---
   if p1a == p2a:
     print("It's a Tie!")
-    runagain()
   elif p1a == 'rock':
     if p2a == 'scissors':
       print(f"{player1} wins!")
-      runagain()
     else:
       print(f"{player2} wins!")
-      runagain()
   elif p1a == 'paper':
     if p2a == 'rock':
       print(f"{player1} wins!")
-      runagain()
     else:
       print(f"{player2} wins!")
-      runagain()
   elif p1a == 'scissors':
     if p2a == 'paper':
       print(f"{player1} wins!")
-      runagain()
     else:
       print(f"{player2} wins!")
-      runagain()
 
-# RUN AGAIN?
-def runagain():
-    run = input("Run again? (Y/N) ").upper()
-    if run == "Y":
-      main()
-      runagain()
-    if run == "N":
-      print("Thank you for playing!")
+# RUN AGAIN? ---
+  def run():
+    while True:
+      playagain = input("Play again? (Y/N): ").upper()
+      if playagain not in ('Y', 'N'):
+        print("Invalid Input.")
+        break
+        run()
+      if playagain == 'Y':
+        main()
+      if playagain == 'N':
+        print("Thank you for playing.")
+        break
+  run()
 
 main()
-runagain()
